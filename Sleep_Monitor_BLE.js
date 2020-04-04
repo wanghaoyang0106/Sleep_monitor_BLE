39ee4d07f639ed19f62a5f4f203f80d554cf983b
wanghaoyang0106 <wanghaoyang0106@outlook.com>
2020-04-04 22:08:10 +0900

    output capacitance value to web; v2.2

--- Sleep_Monitor_BLE.js
+++ Sleep_Monitor_BLE.js
@@ -33,12 +33,10 @@ document.getElementById("start").addEventListener('pointerup', function(event) {
 });
 
 function handleCapacitanceChanged(event) {
-    let CapacitanceValue_little = event.target.value.getFloat32(0, true);
-    let CapacitanceValue_big = event.target.value.getFloat32(0, false);
-    console.log('CapacitanceValue_little:');
-    console.log(CapacitanceValue_little);
-    console.log('CapacitanceValue_big:');
-    console.log(CapacitanceValue_big);
+    let CapacitanceValue = event.target.value.getFloat32(0, true);
+    console.log('CapacitanceValue:');
+    console.log(CapacitanceValue);
+    document.write(CapacitanceValue);
 }
 
 function onDisconnected() {

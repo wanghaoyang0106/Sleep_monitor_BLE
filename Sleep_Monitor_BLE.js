7369d5cba206a191fd601d9527767064c6742e32
wanghaoyang0106 <wanghaoyang0106@outlook.com>
2020-04-05 12:16:37 +0900

    add chart

--- Sleep_Monitor_BLE.js
+++ Sleep_Monitor_BLE.js
@@ -3,6 +3,9 @@ var capacitanceService;
 var capacitanceTime;
 var capacitanceValue;
 
+var capacitanceTime_plot = 0;
+var capacitanceValue_plot = 0;
+
 document.getElementById("start").addEventListener('pointerup', function(event) {
     navigator.bluetooth.requestDevice({ // scan for the designated BLE peripheral
         filters: [{
@@ -30,6 +33,9 @@ document.getElementById("start").addEventListener('pointerup', function(event) {
             capacitanceTime.addEventListener('characteristicvaluechanged',
             handleCapacitanceChanged);
         })
+        .then(characteristic => {
+            capacitanceTime_plot = characteristic.value.getUint32(0, true);
+        })
         .catch(error => { console.log(error); });
 
         console.log('Getting Value Characteristic...');
@@ -46,10 +52,79 @@ document.getElementById("start").addEventListener('pointerup', function(event) {
 function handleCapacitanceChanged(event) {
     let time = event.target.value.getUint32(0, true);
     let val = capacitanceValue.value.getFloat32(0, true);
-    document.write(time, "&#9", val, "<br>");
+    //document.write(time, "&#9", val, "<br>");
+    console.log(val);
+    capacitanceTime_plot = time;
+    capacitanceValue_plot = val;
 }
 
 function onDisconnected() {
     console.log('> Bluetooth Device disconnected');
     // TODO: reconnect
 }
+
+// plot chart
+Highcharts.setOptions({
+	global: {
+		useUTC: false
+	}
+});
+function activeLastPointToolip(chart) {
+	var points = chart.series[0].points;
+	chart.tooltip.refresh(points[points.length -1]);
+}
+var chart = Highcharts.chart('container', {
+	chart: {
+		type: 'spline',
+		marginRight: 10,
+		events: {
+			load: function () {
+				var series = this.series[0],
+					chart = this;
+				activeLastPointToolip(chart);
+				setInterval(function () {
+					series.addPoint([capacitanceTime_plot, capacitanceValue_plot], true, true);
+				}, 500);
+			}
+		}
+	},
+	title: {
+		text: 'capacitance change'
+	},
+	xAxis: {
+		title: {
+            text: 'time(ms)'
+        },
+		tickPixelInterval: 200
+	},
+	yAxis: {
+		title: {
+			text: 'capacitance(pF)'
+		}
+	},
+	tooltip: {
+		formatter: function () {
+			return '<b>' + this.series.name + '</b><br/>' + 'time:' +
+				this.x + '<br/>' + 'value:' +
+				Highcharts.numberFormat(this.y, 2);
+		}
+	},
+	legend: {
+		enabled: false
+	},
+	series: [{
+		name: 'capacitance',
+		data: (function () {
+            var data = [],
+                time = capacitanceTime_plot,
+				i;
+			for (i = -119; i <= 0; i += 1) {
+				data.push({
+					x: time + i * 500,
+					y: 0
+				});
+			}
+			return data;
+		}())
+	}]
+});

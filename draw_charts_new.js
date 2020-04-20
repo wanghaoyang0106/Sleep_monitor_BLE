723c5a5a898d3cc802be99d9fcfafefb841ac455
wanghaoyang0106 <wanghaoyang0106@outlook.com>
2020-04-20 19:28:56 +0800

    reconstruct Data class, realized fake data plot; v5.4

--- draw_charts_new.js
+++ draw_charts_new.js
@@ -1,108 +1,182 @@
 //*************************************************************************************************initial
 //data object
 class Data {
-    constructor(chart, option, refresh_time_ms, refresh_data_num, duration_time, get_data, get_data_param) {
+    constructor(chart, option, refresh_time_ms, refresh_data_num, duration_time_s) {
         this.chart = chart;
         this.data = [];
         this.count = 0; // data count
         this.time = 0; // data time
         this.refresh_time_ms = refresh_time_ms; // refresh invertal of the chart; no less than 100
         this.refresh_data_num = refresh_data_num; // refresh data num (data returened and to be plotted once from get_data method); better no more than 18
-        this.data_num = duration_time * 1000 / refresh_time_ms * refresh_data_num;
+        this.data_num = duration_time_s * 1000 / refresh_time_ms * refresh_data_num;
         this.option = option; // chart option
-        this.get_data = get_data;
-        this.get_data_param = get_data_param;
-        this.data_interval_handler;
-        this.draw_interval_handler;
+        this.data_interval_handler = undefined;
+        this.draw_interval_handler = undefined;
+        this.get_data_flag = false;
+        this.get_data = undefined;
+        this.get_data_param = undefined;
     }
 
-    start() {
-        this.start_data_interval();
-        this.start_draw_interval();
+    get_data_assign(self, get_data, get_data_param) {
+        self.get_data = get_data;
+        self.get_data_param = get_data_param;
+        
+        self.get_data_flag = true;
     }
 
-    start_data_interval() {
-        this.data_interval_handler = setInterval(this.data_update, this.refresh_time_ms);
+    start(self) {
+        self.start_data_interval(self);
+        self.start_draw_interval(self);
     }
 
-    start_draw_interval() {
+    start_data_interval(self) {
+        self.data_interval_handler = window.setInterval(self.data_update, self.refresh_time_ms, self);
+    }
+
+    start_draw_interval(self) {
         //initial draw
-        this.draw_whole();
+        self.draw_whole(self);
         //set interval
-        this.draw_interval_handler = setInterval(this.draw_update, this.refresh_time_ms);
+        self.draw_interval_handler = setInterval(self.draw_update, self.refresh_time_ms, self);
     }
 
-    data_update() {
-        let data_list = this.get_data(this.get_data_param);
-        if (data_list.length != this.refresh_data_num) {
+    data_update(self) {
+        if (self.get_data_flag == false) { // get_data method not assigned
+            throw Error('get_data method and parameters are not assigned!');
+        }
+        let data_list = self.get_data(self.get_data_param);
+        if (data_list.length != self.refresh_data_num) { // get_data method not match
             throw Error('get_data method return length dose not match!');
         }
-        for (let i = 0; i < this.refresh_data_num; i++) {
-            this.time_update();
-            this.data.push({
-                count: this.count,
+        for (let i = 0; i < self.refresh_data_num; i++) {
+            self.time_update(self);
+            self.data.push({
+                count: self.count,
                 value: [
-                    this.time,
+                    self.time,
                     data_list[i],
                 ],
             });
-            this.count_update();
+            self.count_update(self);
+            //console.log(data_list[i]);
         }
-        this.trim();
+        self.trim(self);
     }
 
-    time_update() {
-        this.time = this.count * this.refresh_time_ms / 1000.0 / this.refresh_data_num;
+    time_update(self) {
+        self.time = self.count * self.refresh_time_ms / 1000.0 / self.refresh_data_num;
     }
 
-    count_update() {
-        this.count++;
+    count_update(self) {
+        self.count++;
     }
 
-    trim() {
-        while (this.data.length > this.data_num) {
-            this.data.shift()
+    trim(self) {
+        while (self.data.length > self.data_num) {
+            self.data.shift()
         };
     }
 
-    draw_whole() {
+    draw_whole(self) {
         // rewrite some option
-        this.option.xAxis.min = this.data[0].value[0];
-        this.option.xAxis.max = this.data[0].value[0] + this.data_num * this.refresh_time_ms / 1000.0 / this.refresh_data_num;
-        this.option.series.data = this.data;
+        //self.option.xAxis.min = self.data[0].value[0];
+        //self.option.xAxis.max = self.data[0].value[0] + self.data_num * self.refresh_time_ms / 1000.0 / self.refresh_data_num;
+        self.option.series.data = self.data;
         // draw the whole chart
-        this.chart.setOption(this.option);
+        self.chart.setOption(self.option);
     }
 
-    draw_update() {
-        this.chart.setOption({
+    draw_update(self) {
+        self.chart.setOption({
             series: [{
-                data: this.data,
+                data: self.data,
             }],
         });
-        this.chart.setOption({
+        self.chart.setOption({
             xAxis: [{
             type: 'value',
             splitLine: {
                 show: false,
             },
-            min: this.data[0].value[0],
-            max: this.data[0].value[0] + this.data_num * this.refresh_time_ms / 1000.0 / this.refresh_data_num,
+            min: self.data[0].value[0],
+            max: self.data[0].value[0] + self.data_num * self.refresh_time_ms / 1000.0 / self.refresh_data_num,
         }]
         });
     }
 
-    stop() {
-        this.stop_data_interval();
-        this.stop_draw_interval();
+    stop(self) {
+        self.stop_data_interval(self);
+        self.stop_draw_interval(self);
     }
 
-    stop_data_interval() {
-        clearInterval(this.data_interval_handler);
+    stop_data_interval(self) {
+        clearInterval(self.data_interval_handler);
     }
 
-    stop_draw_interval() {
-        clearInterval(this.draw_interval_handler);
+    stop_draw_interval(self) {
+        clearInterval(self.draw_interval_handler);
+    }
+
+    reset(self) {
+        self.data = [];
+        self.count = 0; // data count
+        self.time = 0; // data time
     }
 }
 
+//*************************************************************************************************get chart and data initialize
+var data_list = [];
+
+var chart_1 = echarts.init(document.getElementById('chart_1'));
+var option_1 = { // chart option
+    title: {
+        text: 'Data 1',
+    },
+    xAxis: {
+        type: 'value',
+        name: 'time(s)',
+    },
+    yAxis: {
+        type: 'value',
+        name: 'value',
+        //min: 0,
+        //max: 1,
+    },
+    series: [{
+        name: 'data 1',
+        type: 'line',
+        showSymbol: false,
+        hoverAnimation: false,
+    }]
+};
+var data_1 = new Data(chart_1, option_1, 100, 1, 10);
+data_list.push(data_1);
+
+//*************************************************************************************************assign data update method: fake data
+// set button click event
+document.getElementById("fake").onclick = fake_data;
+
+function fake_data() {
+    for (let i = 0; i < data_list.length; i++) {
+        let data = data_list[i];
+        data.stop(data); // stop everything currently running
+        data.reset(data); // reset data
+        data.get_data_assign(data, fake_sin, []); // assign data update method
+        data.start(data); // start update data and draw
+    }
+}
+
+function fake_sin(param) {
+    return [Math.sin(Date.now() / 1000.0 * 6.28) / 2 + 0.5];
+}
+
+//*************************************************************************************************stop
+// set button click event
+document.getElementById("stop").onclick = stop;
+
+function stop() {
+    for (let i = 0; i < data_list.length; i++) {
+        let data = data_list[i];
+        data.stop(data); // stop everything currently running
+    }
+}
--- test.js
+++
@@ -1,8 +0,0 @@
-var data = [];
-var get = [1,2,3,4,5,6]
-
-for (var i = 0; i < 6; i++){
-    data.push(get[i]);
-
-}
-console.log(data);
